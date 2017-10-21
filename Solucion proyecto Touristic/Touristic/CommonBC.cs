2c99fac88bfc9658482a3eba4645481a295e3b5f
Diego Lufi Padilla <diego.lufi@gmail.com>
2017-10-21 18:47:54 -0300

    Acceso a BDD
    Creacion del CommonBC y EDM

---
+++ Solucion proyecto Touristic/Touristic/CommonBC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Touristic.DALC;
+
+namespace Touristic
+{
+    public static class CommonBC
+    {
+        private static TouristicEntities _baseDeDatos;
+
+        public static TouristicEntities BaseDeDatos
+        {
+            get
+            {
+                if (_baseDeDatos == null)
+                {
+                    _baseDeDatos = new TouristicEntities();
+                }
+
+                return _baseDeDatos;
+            }
+        }
+    }
+}
